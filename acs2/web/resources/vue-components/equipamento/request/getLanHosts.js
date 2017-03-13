39e39c234c66d00b02c70971c41d87f10613f6b7
Agibert <matheusagibert@gmail.com>
2017-03-13 19:20:58 -0300

    wanDiagnostic done

--- acs2/web/resources/vue-components/equipamento/request/getLanHosts.js
+++ acs2/web/resources/vue-components/equipamento/request/getLanHosts.js
@@ -23,12 +23,6 @@ Vue.component("getLanHosts", {
                 return new Equipamento(this.eqpString);
             }
         },
-        info: {
-            type: WifiInfo,
-            default: function() {
-                return new WifiInfo();
-            }
-        },
         alertPanel: {
             type: Object
         }
