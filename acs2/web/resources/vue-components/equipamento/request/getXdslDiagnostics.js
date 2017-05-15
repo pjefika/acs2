bffe735f2b0ce77b693d13844bb0ddf60b3b0c07
FabioClem <fabio.henrique.clem@gmail.com>
2017-05-15 10:35:20 -0300

    Corrigido estrutura do modal getXdslDiagnostics retirado modal-footer de dentro do modal-body

--- acs2/web/resources/vue-components/equipamento/request/getXdslDiagnostics.js
+++ acs2/web/resources/vue-components/equipamento/request/getXdslDiagnostics.js
@@ -63,93 +63,94 @@ Vue.component("getXdslDiagnostics", {
     mounted: function() {
         this.getXdslDiagnostics();
     },
-    template: "\
-            <div class='modal-body'>\n\
-                <component is='alertpanel' :mensagem='mensagem' :erro='erro'></component>\n\
-                <div class='row'>\n\
-                    <div class='col-md-6'>\n\
-                        <table class='table table-bordered small'>\n\
-                            <tbody>\n\
-                                <tr>\n\
-                                    <td><label>Modulação</label></td>\n\
-                                    <td>{{info.ModulationType}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>ShowtimeStart</label></td>\n\
-                                    <td>{{info.ShowtimeStart}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>LinkRetrain</label></td>\n\
-                                    <td>{{info.LinkRetrain}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>LossOfFraming</label></td>\n\
-                                    <td>{{info.LossOfFraming}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>SeverelyErroredSecs</label></td>\n\
-                                    <td>{{info.SeverelyErroredSecs}}</td>\n\
-                                </tr>\n\
-                            </tbody>\n\
-                        </table>\n\
+    template: "<div>\n\
+                    <component is='alertpanel' :mensagem='mensagem' :erro='erro'></component>\n\
+                    <div class='modal-body'>\n\
+                        <div class='row'>\n\
+                            <div class='col-md-6'>\n\
+                                <table class='table table-bordered small'>\n\
+                                    <tbody>\n\
+                                        <tr>\n\
+                                            <td><label>Modulação</label></td>\n\
+                                            <td>{{info.ModulationType}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>ShowtimeStart</label></td>\n\
+                                            <td>{{info.ShowtimeStart}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>LinkRetrain</label></td>\n\
+                                            <td>{{info.LinkRetrain}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>LossOfFraming</label></td>\n\
+                                            <td>{{info.LossOfFraming}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>SeverelyErroredSecs</label></td>\n\
+                                            <td>{{info.SeverelyErroredSecs}}</td>\n\
+                                        </tr>\n\
+                                    </tbody>\n\
+                                </table>\n\
+                            </div>\n\
+                            <div class='col-md-6'>\n\
+                                <table class='table table-bordered small'>\n\
+                                    <thead>\n\
+                                        <tr>\n\
+                                            <th></th>\n\
+                                            <th>Downstream</th>\n\
+                                            <th>Upstream</th>\n\
+                                        </tr>\n\
+                                    </thead>\n\
+                                    <tbody>\n\
+                                        <tr>\n\
+                                            <td><label>Vel. Máx. Alcançável</label></td>\n\
+                                            <td>{{info.DownstreamMaxRate}}</td>\n\
+                                            <td>{{info.UpstreamMaxRate}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>Vel. Sincronizada</label></td>\n\
+                                            <td>{{info.DownstreamCurrRate}}</td>\n\
+                                            <td>{{info.UpstreamCurrRate}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>Snr</label></td>\n\
+                                            <td>{{info.DownstreamNoiseMargin}}</td>\n\
+                                            <td>{{info.UpstreamNoiseMargin}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>Atn</label></td>\n\
+                                            <td>{{info.DownstreamAttenuation}}</td>\n\
+                                            <td>{{info.UpstreamAttenuation}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>Power</label></td>\n\
+                                            <td>{{info.DownstreamPower}}</td>\n\
+                                            <td>{{info.UpstreamPower}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>FEC</label></td>\n\
+                                            <td>{{info.FECErrors}}</td>\n\
+                                            <td>{{info.ATUCFECErrors}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>HEC</label></td>\n\
+                                            <td>{{info.HECErrors}}</td>\n\
+                                            <td>{{info.ATUCHECErrors}}</td>\n\
+                                        </tr>\n\
+                                        <tr>\n\
+                                            <td><label>CRC</label></td>\n\
+                                            <td>{{info.CRCErrors}}</td>\n\
+                                            <td>{{info.ATUCCRCErrors}}</td>\n\
+                                        </tr>\n\
+                                    </tbody>\n\
+                                </table>\n\
+                            </div>\n\
+                        </div>\n\
                     </div>\n\
-                    <div class='col-md-6'>\n\
-                        <table class='table table-bordered small'>\n\
-                            <thead>\n\
-                                <tr>\n\
-                                    <th></th>\n\
-                                    <th>Downstream</th>\n\
-                                    <th>Upstream</th>\n\
-                                </tr>\n\
-                            </thead>\n\
-                            <tbody>\n\
-                                <tr>\n\
-                                    <td><label>Vel. Máx. Alcançável</label></td>\n\
-                                    <td>{{info.DownstreamMaxRate}}</td>\n\
-                                    <td>{{info.UpstreamMaxRate}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>Vel. Sincronizada</label></td>\n\
-                                    <td>{{info.DownstreamCurrRate}}</td>\n\
-                                    <td>{{info.UpstreamCurrRate}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>Snr</label></td>\n\
-                                    <td>{{info.DownstreamNoiseMargin}}</td>\n\
-                                    <td>{{info.UpstreamNoiseMargin}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>Atn</label></td>\n\
-                                    <td>{{info.DownstreamAttenuation}}</td>\n\
-                                    <td>{{info.UpstreamAttenuation}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>Power</label></td>\n\
-                                    <td>{{info.DownstreamPower}}</td>\n\
-                                    <td>{{info.UpstreamPower}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>FEC</label></td>\n\
-                                    <td>{{info.FECErrors}}</td>\n\
-                                    <td>{{info.ATUCFECErrors}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>HEC</label></td>\n\
-                                    <td>{{info.HECErrors}}</td>\n\
-                                    <td>{{info.ATUCHECErrors}}</td>\n\
-                                </tr>\n\
-                                <tr>\n\
-                                    <td><label>CRC</label></td>\n\
-                                    <td>{{info.CRCErrors}}</td>\n\
-                                    <td>{{info.ATUCCRCErrors}}</td>\n\
-                                </tr>\n\
-                            </tbody>\n\
-                        </table>\n\
+                    <div class='modal-footer'>\n\
+                        <button type='button' class='btn btn-default' data-dismiss='modal'>Fechar</button>\n\
+                        <button type='button' class='btn btn-primary' @click='getXdslDiagnostics()'>Consultar</button>\n\
                     </div>\n\
-                </div>\n\
-                <div class='modal-footer'>\n\
-                    <button type='button' class='btn btn-default' data-dismiss='modal'>Fechar</button>\n\
-                    <button type='button' class='btn btn-primary' @click='getXdslDiagnostics()'>Consultar</button>\n\
-                </div>\n\
-            </div>"
+                </div>"
 });
