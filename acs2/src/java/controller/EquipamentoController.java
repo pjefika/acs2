3c1bafaaf3bf210598c4c26f099512b7f7b40c40
Henrique Merlin <henmerlin@gmail.com>
2017-08-07 15:38:39 -0300

    add routes + update cors

--- acs2/src/java/controller/EquipamentoController.java
+++ acs2/src/java/controller/EquipamentoController.java
@@ -6,13 +6,19 @@
 package controller;
 
 import dao.factory.FactoryDAO;
+import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import model.device.interfacestatistics.InterfaceStatistics;
+import model.device.lanhost.LanDevice;
+import model.device.pppoe.PPPoECredentialsInfo;
+import model.device.wan.WanInfo;
 import model.device.wifi.WifiInfoFull;
+import model.device.xdsldiagnostics.XdslDiagnostics;
 import model.service.dto.DetailOut;
 import model.entity.LogEntity;
 import model.log.AcaoAcsEnum;
@@ -60,6 +66,145 @@ public class EquipamentoController extends RestAbstractController {
             WifiInfoFull wifi = FactoryDAO.createSynch().getWifiInfoFull(in.getDevice());
             l.setSaida(wifi);
             return ok(wifi);
+        } catch (Exception e) {
+            l.setSaida(e.getMessage());
+            return internalServerError(e);
+        } finally {
+            try {
+                FactoryDAO.createLogDAO().cadastrar(l);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    @POST
+    @Path("/getLanHosts")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getLanHosts(GetDeviceDataIn in) {
+        in.setAcao(AcaoAcsEnum.GET_LAN_HOSTS);
+        LogEntity l = in.create();
+        try {
+            List<LanDevice> lst = FactoryDAO.createSynch().getLanHosts(in.getDevice());
+            l.setSaida(lst);
+            return ok(lst);
+        } catch (Exception e) {
+            l.setSaida(e.getMessage());
+            return internalServerError(e);
+        } finally {
+            try {
+                FactoryDAO.createLogDAO().cadastrar(l);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    @POST
+    @Path("/getWanInfo")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getWanInfo(GetDeviceDataIn in) {
+        in.setAcao(AcaoAcsEnum.GET_WAN_INFO);
+        LogEntity l = in.create();
+        try {
+            WanInfo w = FactoryDAO.createSynch().getWanInfo(in.getDevice());
+            l.setSaida(w);
+            return ok(w);
+        } catch (Exception e) {
+            l.setSaida(e.getMessage());
+            return internalServerError(e);
+        } finally {
+            try {
+                FactoryDAO.createLogDAO().cadastrar(l);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    @POST
+    @Path("/getInterfaceStatistics")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getInterfaceStatistics(GetDeviceDataIn in) {
+        in.setAcao(AcaoAcsEnum.GET_INTERFACE_STATISTICS);
+        LogEntity l = in.create();
+        try {
+            List<InterfaceStatistics> w = FactoryDAO.createSynch().getInterfaceStatistics(in.getDevice());
+            l.setSaida(w);
+            return ok(w);
+        } catch (Exception e) {
+            l.setSaida(e.getMessage());
+            return internalServerError(e);
+        } finally {
+            try {
+                FactoryDAO.createLogDAO().cadastrar(l);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    @POST
+    @Path("/getXdslDiagnostic")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getXdslDiagnostic(GetDeviceDataIn in) {
+        in.setAcao(AcaoAcsEnum.GET_XDSL_DIAGNOSTIC);
+        LogEntity l = in.create();
+        try {
+            XdslDiagnostics w = FactoryDAO.createSynch().getXdslDiagnostic(in.getDevice());
+            l.setSaida(w);
+            return ok(w);
+        } catch (Exception e) {
+            l.setSaida(e.getMessage());
+            return internalServerError(e);
+        } finally {
+            try {
+                FactoryDAO.createLogDAO().cadastrar(l);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    @POST
+    @Path("/getPPPoECredentials")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getPPPoECredentials(GetDeviceDataIn in) {
+        in.setAcao(AcaoAcsEnum.GET_PPPOE_CREDENTIALS);
+        LogEntity l = in.create();
+        try {
+            PPPoECredentialsInfo w = FactoryDAO.createSynch().getPPPoECredentials(in.getDevice());
+            l.setSaida(w);
+            return ok(w);
+        } catch (Exception e) {
+            l.setSaida(e.getMessage());
+            return internalServerError(e);
+        } finally {
+            try {
+                FactoryDAO.createLogDAO().cadastrar(l);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    
+    @POST
+    @Path("/reboot")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response reboot(GetDeviceDataIn in) {
+        in.setAcao(AcaoAcsEnum.GET_PPPOE_CREDENTIALS);
+        LogEntity l = in.create();
+        try {
+            PPPoECredentialsInfo w = FactoryDAO.createSynch().getPPPoECredentials(in.getDevice());
+            l.setSaida(w);
+            return ok(w);
         } catch (Exception e) {
             l.setSaida(e.getMessage());
             return internalServerError(e);
