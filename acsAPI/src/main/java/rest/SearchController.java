ab9c533f60e2231e0a5207900090264a8f193bcf
Agibert <matheusagibert@gmail.com>
2018-04-16 14:51:02 -0300

    fix controllers

--- acsAPI/src/main/java/rest/SearchController.java
+++ acsAPI/src/main/java/rest/SearchController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller;
+package rest;
 
 import com.alcatel.hdm.service.nbi2.NbiDeviceData;
 import dao.factory.FactoryDAO;
