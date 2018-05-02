fcc63614abd9136b1860ecd228319d529dbd5a45
Agibert <matheusagibert@gmail.com>
2018-05-02 11:39:32 -0300

    update

--- acsAPI/src/main/java/parser/device/PPPoECredentialsInfoSerializer.java
+++ acsAPI/src/main/java/parser/device/PPPoECredentialsInfoSerializer.java
@@ -32,8 +32,8 @@ public class PPPoECredentialsInfoSerializer extends StdSerializer<PPPoECredentia
             throws IOException, JsonProcessingException {
 
         jgen.writeStartObject();
-        jgen.writeStringField("username", value.getUsername());
-        jgen.writeStringField("password", value.getPassword());
+        jgen.writeStringField("Username", value.getUsername());
+        jgen.writeStringField("Password", value.getPassword());
 
         jgen.writeEndObject();
     }
