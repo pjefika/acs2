a8535a5a7f23cfae614387678c8999da935c92e7
A0077749 <A0077749@NPRCURVHADMCV5Q.mshome.net>
2018-08-13 10:36:41 -0300

    Adicionado busca versão firmware

--- acsAPI/src/main/java/rest/EquipamentoController.java
+++ acsAPI/src/main/java/rest/EquipamentoController.java
@@ -22,6 +22,7 @@ import dao.factory.FactoryDAO;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -849,4 +850,5 @@ public class EquipamentoController extends RestAbstractController {
             }
         }
     }
+
 }
